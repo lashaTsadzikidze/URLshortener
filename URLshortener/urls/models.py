f2fc45f7d7a2e20991655ef1c5495e00fecf121a
lasha1222 <lasha.tsadzikidze@geolab.edu.ge>
2024-10-24 18:10:42 +0400

    add model for url

--- URLshortener/urls/models.py
+++ URLshortener/urls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class URL(models.Model):
+    long_url = models.CharField()
+    short_url = models.CharField(max_length=100, unique=True)
+    visit_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.short_url} - > {self.long_url}'

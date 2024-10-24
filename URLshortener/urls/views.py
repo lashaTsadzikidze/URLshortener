e283bd642e92d7214b884af7c9c0f7068156ad0c
lasha1222 <lasha.tsadzikidze@geolab.edu.ge>
2024-10-24 18:31:19 +0400

    add template for main page

--- URLshortener/urls/views.py
+++ URLshortener/urls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import URL
+import random
+import string
 
 # Create your views here.
+def index(request):
+
+    return render(request, 'index.html')

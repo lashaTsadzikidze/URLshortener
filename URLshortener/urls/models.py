b6ee8ccd6c50fc754324676733c5c16eafbfd9df
lasha1222 <lasha.tsadzikidze@geolab.edu.ge>
2024-10-24 18:14:24 +0400

    make migrations

--- URLshortener/urls/models.py
+++ URLshortener/urls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class URL(models.Model):
-    long_url = models.CharField()
+    long_url = models.CharField(max_length=200)
     short_url = models.CharField(max_length=100, unique=True)
     visit_count = models.IntegerField(default=0)
 
